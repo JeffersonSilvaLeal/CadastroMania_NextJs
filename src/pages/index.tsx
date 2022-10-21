de8ecb364a32854f7d1b659fd017afa0aa521b42
Jefferson <jefferson.leal@7comm.com.br>
2022-10-21 16:42:29 -0300

    Componente Tabela

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,27 @@
 import React from "react";
 import Layout from "../components/Layout";
+import Tabela from "../components/Tabela";
+import Cliente from "../core/Cliente";
 
 export default function Home() {
+
+  const clientes = [
+    new Cliente('Jefferson', 29, '1'),
+    new Cliente('Taian', 19, '2'),
+    new Cliente('Jader', 39, '3'),
+    new Cliente('Lucia', 35, '4'),
+  ]
+
+
+  function clienteSelecionado(cliente: Cliente) {
+
+  }
+
+  function clienteExcluido(cliente: Cliente) {
+
+  }
+
+
   return (
     <div className={`
       flex justify-center items-center h-screen
@@ -9,7 +29,10 @@ export default function Home() {
       text-white
     `}>
       <Layout titulo="Cadastro Simples">
-        <span>Conteudo</span>
+        <Tabela clientes={clientes} 
+        clienteSelecionado={clienteSelecionado}
+        clienteExcluido={clienteExcluido}/>
+
       </Layout>
     </div>
   )
